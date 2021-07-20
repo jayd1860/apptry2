b75ba95c2ca3abf6c210d1a7a71eca9eeaed0dbb
Jay Dubb <jayd1860@gmail.com>
2021-07-19 21:24:59 -0400

    ujjjjj

--- Utils/funcCommonDiff.m
+++ Utils/funcCommonDiff.m
@@ -1,37 +1,4 @@
-function [out1, out2] = funcCommonDiff(inp1, inp2)
-out1 = [];
-out2 = [];
-c = context();
-if isempty(c)
-    return;
-end
-if strcmp(c, 'apptry1')
-    if nargin == 0
-        [out1] = funcCommonDiff_apptry1();
-    elseif nargin == 1
-        [out1] = funcCommonDiff_apptry1(inp1);
-    end
-elseif strcmp(c, 'apptry2')
-    if nargin == 0
-        [out1, out2] = funcCommonDiff_apptry2();
-    elseif nargin == 1
-        [out1, out2] = funcCommonDiff_apptry2(inp1);
-    elseif nargin == 2
-        [out1, out2] = funcCommonDiff_apptry2(inp1, inp2);
-    end
-end
-
-
-% ---------------------------------------------------------
-function [y] = funcCommonDiff_apptry1(x)
-y = x+666;
-
-
-
-
-
-% ---------------------------------------------------------
-function [v, w] = funcCommonDiff_apptry2(x, y)
+function [v, w] = funcCommonDiff(x, y)
 if nargin==1
     y = 33;
 end
--- Utils/getAppDir.m
+++ Utils/getAppDir.m
@@ -1,30 +1,4 @@
-function [out1] = getAppDir()
-out1 = [];
-c = context();
-if isempty(c)
-    return;
-end
-if strcmp(c, 'apptry1')
-    if nargin == 0
-        [out1] = getAppDir_apptry1();
-    end
-elseif strcmp(c, 'apptry2')
-    if nargin == 0
-        [out1] = getAppDir_apptry2();
-    end
-end
-
-
-% ---------------------------------------------------------
-function [p] = getAppDir_apptry1()
-
-d = which('Apptry1GUI');
-p = fileparts(d);
-
-
-
-% ---------------------------------------------------------
-function [p] = getAppDir_apptry2()
+function p = getAppDir()
 
 d = which('Apptry2GUI');
 p = fileparts(d);
